ebee5543e4d06351d60645f19b19ab2723181c54
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-23 06:18:48 -0400

    add requirement for using redux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose ;
+const users = [];
+const reducer = combineReducers({
+  users
+})
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
