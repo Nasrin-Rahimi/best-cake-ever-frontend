32245b5ed5bda19bea51601c3b2de206e584d26e
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-23 10:01:20 -0400

    change the name of user to customer to be the same as backend customer model

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore, combineReducers } from 'redux';
-import userReducer from './reducers/users';
+import customerReducer from './reducers/customers';
 
 const reducer = combineReducers({
-    users: userReducer
+    customers: customerReducer
   })
   
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
