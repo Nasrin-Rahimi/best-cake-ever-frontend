ef9d6f59165ef38afa0ec2ca24f246442869f3e1
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-24 13:39:23 -0400

    update login state

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { updateLoginForm } from '../actions/loginForm';
 
-const Login = (props) => {
+const Login = ({loginForm, updateLoginForm}) => {
+
+    const handleInputChange = event => {
+        const { name, value } = event.target //get dynamically name and value
+        const updatedFormInfo = {
+            ...loginForm,
+            [name]: value
+        }
+        updateLoginForm(updatedFormInfo)
+    }
+
     return (
         <form onSubmit="">
-            <input value={props.name} type="text" name="name" placeholder="Name" onChange="" />
-            <input value={props.password} type="text" name="password" placeholder="Password" onChange="" />
+            <input value={loginForm.name} type="text" name="name"
+             placeholder="Name" onChange={handleInputChange} />
+            <input value={loginForm.password} type="text" name="password"
+             placeholder="Password" onChange={handleInputChange} />
             <input type="button" value="Log In" />
         </form>
     )
@@ -15,11 +28,10 @@ const Login = (props) => {
 //gives us access to these chunk of state from the store as props 
 const mapStateToProps = state => {
     return {
-        name: state.loginForm.name,
-        password: state.loginForm.password
+        loginForm: state.loginForm
     }
 }
 
 //connect is a function that takes up to 4 arguments we usually deal with first 2. 
 //It returns a function that takes a component and then returns a component
-export default connect(mapStateToProps)(Login)
+export default connect(mapStateToProps, { updateLoginForm })(Login)
