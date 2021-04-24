5d03730a0887f1ce08031688c96823cda4c90768
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-24 09:34:19 -0400

    add reducer and action for login

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import customerReducer from './reducers/customers';
 import currentCustomer from './reducers/currentCustomer';
+import loginForm from './reducers/loginForm'
 
 const reducer = combineReducers({
     customers: customerReducer,
-    currentCustomer //OR currentCustomer: currentCustomer
+    currentCustomer, //OR currentCustomer: currentCustomer
+    loginForm
   })
   
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
