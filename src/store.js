5c5914f4f910301415c48bb919132ceb5d8fe0e9
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-24 05:31:46 -0400

    add currentCustomer reducer and add it to the store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import customerReducer from './reducers/customers';
+import currentCustomer from './reducers/currentCustomer';
 
 const reducer = combineReducers({
-    customers: customerReducer
+    customers: customerReducer,
+    currentCustomer //OR currentCustomer: currentCustomer
   })
   
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
