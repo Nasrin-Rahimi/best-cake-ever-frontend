ccf0cf9a663cb76fc7058b99b49da0dd5a88bc4d
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-25 06:38:24 -0400

    use applyMiddleware to create store

--- src/store.js
+++ src/store.js
@@ -1,14 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import customerReducer from './reducers/customers';
 import currentCustomer from './reducers/currentCustomer';
-import loginForm from './reducers/loginForm'
+import loginForm from './reducers/loginForm';
+import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
     customers: customerReducer,
     currentCustomer, //OR currentCustomer: currentCustomer
-    loginForm
+    loginForm 
   })
   
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 export default store;
