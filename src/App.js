d375d810f5ec874240d53849d455b94dc510b7fc
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 09:52:24 -0400

    remove login/logout from app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
-import Logout from './components/Logout';
 import { connect } from 'react-redux';
 import { getCurrentCustomer } from './actions/currentCustomer';
+import NavBar from './components/NavBar';
 
 class App extends React.Component {
 
@@ -19,26 +18,13 @@ class App extends React.Component {
         <header className="App-header">
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" ></a>
         </header>
-        <h1>Welcome to Best Cake Ever</h1>
+        <h1>Best Cake Ever</h1>
 
-        { this.props.currentCustomer ? <Logout /> : <Login /> }
+        <NavBar />
 
       </div>
     );
   }
 }
 
-//destructing currentCustomer from state. We can do it because
-//the incoming argument is as object (state) comign from redux
-//and we know it has a property called currentCustomer
-const mapStateToProps = ( {currentCustomer} ) => {
-  return {
-    currentCustomer
-  }
-}
-
-export default connect(mapStateToProps, { getCurrentCustomer })(App);
-
-//For adding logout, we need to know about current customer
-//so we need to add mapStateToProps to get state of current
-//customer
+export default connect(null, { getCurrentCustomer })(App);
