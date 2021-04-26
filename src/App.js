3566aa9d6c8bb4064e30b81a6b8a4aca28c00ebd
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 05:45:23 -0400

    get current customer in componentdidMount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React from 'react';
 import './App.css';
-import Login from './components/Login'
+import Login from './components/Login';
+import { connect } from 'react-redux';
+import { getCurrentCustomer } from './actions/currentCustomer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-      <h1>Welcome to Best Cake Ever</h1>
-      <Login />
-    </div>
-  );
+class App extends React.Component {
+
+  //every time app did mount, we send a request and say is there anyone logged in
+  //if yes we grab it
+  componentDidMount() {
+    this.props.getCurrentCustomer()
+  }
+
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            
+          </a>
+        </header>
+        <h1>Welcome to Best Cake Ever</h1>
+        <Login />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, { getCurrentCustomer })(App);
