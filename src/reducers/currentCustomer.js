991a751f3ca19762fa91d47428dde0212d689a01
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 07:57:47 -0400

    add case for clear current customer

--- src/reducers/currentCustomer.js
+++ src/reducers/currentCustomer.js
@@ -2,6 +2,8 @@ export default (state = null, action) => {
     switch (action.type) {
         case "SET_CURRENT_CUSTOMER":
             return action.customer
+        case "CLEAR_CURRENT_CUSTOMER":
+            return null
         default: 
             return state
     }
