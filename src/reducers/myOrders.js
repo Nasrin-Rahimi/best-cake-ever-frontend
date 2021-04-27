f103b99ff3d2c1808030ceb443e5026dda96f14b
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-27 11:11:59 -0400

    get customer orders from fetch

--- src/reducers/myOrders.js
+++ src/reducers/myOrders.js
@@ -1,5 +1,7 @@
 export default (state = [], action) => {
     switch (action.type) {
+        case "SET_MY_ORDERS":
+            return action.orders
         default: 
             return state
     }
