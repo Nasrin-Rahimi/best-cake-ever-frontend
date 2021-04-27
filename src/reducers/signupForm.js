60eca5087a25b7ac0f51cb6fb105ce367052579a
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-27 19:55:31 -0400

    reducer for signup form

---
+++ src/reducers/signupForm.js
@@ -0,0 +1,11 @@
+const initialState = {
+    name: "",
+    password: ""
+}
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
