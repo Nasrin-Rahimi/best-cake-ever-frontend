b9ebbc40e3596ce3afbe02176aed03c46ac1a30e
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-28 10:03:30 -0400

    add app logo and add style for it

--- src/App.js
+++ src/App.js
@@ -22,7 +22,10 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Best Cake Ever</h1>
+          <a href="/">
+            <img src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX25762110.jpg" alt="Best Cake Ever" class="header_img" />
+          </a>
+          {/* <h1>Best Cake Ever</h1> */}
         </header>
         
         <NavBar />
