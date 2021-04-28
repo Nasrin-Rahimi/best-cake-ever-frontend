4306ed8b5ec771a7e8301986a5ff74cbbc04b4c0
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-28 05:16:32 -0400

    reducer for update signup form

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -5,6 +5,8 @@ const initialState = {
 
 export default (state=initialState, action) => {
     switch (action.type) {
+        case "UPDATE_SIGNUP_FORM":
+            return action.formData
         default:
             return state
     }
