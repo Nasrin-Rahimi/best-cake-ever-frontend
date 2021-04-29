c1c49564ff04de0f9004cdab001a586eb6f72a00
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-29 15:11:29 -0400

    filter every category's products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ProductCard from './ProductCard';
 
-const Products = () => {
+const Products = (props) => {
+
+    const category_id = props.match.params.category_id
+    const categoryProducts = props.Products.filter(product => product.attributes.category_id == category_id)
+    const productCards = categoryProducts.map(product => <ProductCard product={product} key={product.id} />)
+
     return (
-        <p>Products</p>
+        <div className="Products" >
+            {console.log(props)}
+            {productCards}
+        </div>
     )
 }
 
-export default Products
+const mapStateToProps = state => {
+    return {
+        Products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(Products)
