4f5a5e54b489c005c52f43151aaa9ed5bedb7e13
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-29 10:00:28 -0400

    getCategories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import CategoryCard from './CategoryCard';
+import { getCategories } from '../actions/categories';
 
-const Categories = () => {
+const Categories = (props) => {
+
+    props.getCategories()
+    
+    const categoryCards = props.Categories.map(category => <CategoryCard category={category} key={category.id} />)
+
     return(
         <div>
-            Categories
+            {categoryCards}
         </div>
     )
 }
+
+const mapStateToProps = state => {
+    return {
+        Categories: state.categories
+    }
+}
+
+export default connect(mapStateToProps, { getCategories })(Categories)
 
