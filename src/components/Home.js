56728f499f8f27214c67c6812ecb5f5d1f14c776
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-29 11:25:10 -0400

    add image to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Categories from './Categories';
+// import Categories from './Categories';
 
 const Home = () => {
     return(
-        <div>
+        <div className="home">
             <h2>Welcome to Best Cake Ever</h2>
-            <Categories />
+            <img src="https://qph.fs.quoracdn.net/main-qimg-0b1a8b43905929eaa06e475a29841443" className="homeImage" />
+            {/* <Categories /> */}
         </div>
     )
 }
