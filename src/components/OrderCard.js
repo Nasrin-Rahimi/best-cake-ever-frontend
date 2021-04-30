3d9ff959cbceb3a3574805c940700830c86702e4
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-30 07:48:38 -0400

    implementing order details link by clicking on order

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -1,8 +1,22 @@
-import React from 'react';
+
+import React,{useState} from 'react';
 
 const OrderCard = ({ order }) => {
+    console.log(order.relationships.orderdetails.data)
+    const [orderDetails,setOrderDetails]=useState(false);
+    const extraContent=<div>
+      <p className="OrderDetail">
+        Order Detail
+      </p>
+  </div>
+
+
     return (
-        <p>Order Date: {order.attributes.order_Date}</p>
+        <div className="test">
+        <a className="OrderDetailsLink" onClick={()=>{setOrderDetails(!orderDetails)}}><h3>{order.attributes.order_Date}</h3></a>
+      {orderDetails && extraContent}
+      </div>
+        // <p>Order Date: {order.attributes.order_Date}</p>
     )
 }
 
