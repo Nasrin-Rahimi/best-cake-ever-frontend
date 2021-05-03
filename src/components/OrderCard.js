647da7f6e243b3943b0260fd2208ece37acae734
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-05-03 05:17:25 -0400

    delete comment line

--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -4,29 +4,27 @@ import MyOrderDetails from './MyOrderDetails';
 
 const OrderCard = ({ order }) => {
 
-    const [orderDetails,setOrderDetails]=useState(false);
+  const [orderDetails,setOrderDetails]=useState(false);
 
-    const extraContent =
+  const extraContent =
     <div>
       <p className="OrderDetail">
         <MyOrderDetails orderId = {order.id} />
       </p>
     </div>
 
+  return (
+    <div className="OrderCard">
+      <a className="OrderDetailsLink" onClick={()=>{setOrderDetails(!orderDetails)}}>
+        <h2>
+          {order.attributes.order_Date} - 
+          ${order.attributes.total_price}
+        </h2>
 
-    return (
-      <div className="OrderCard">
-        <a className="OrderDetailsLink" onClick={()=>{setOrderDetails(!orderDetails)}}>
-          <h2>
-            {order.attributes.order_Date} - 
-            ${order.attributes.total_price}
-          </h2>
-
-        </a>
-      {orderDetails && extraContent}
-      </div>
-        // <p>Order Date: {order.attributes.order_Date}</p>
-    )
+      </a>
+    {orderDetails && extraContent}
+    </div>
+  )
 }
 
 export default OrderCard
