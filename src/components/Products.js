2a0c63abbcee7e23ca1e836a36cde371105d330c
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-05-03 04:57:12 -0400

    add a message when category doesn't have product

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,10 +8,11 @@ const Products = (props) => {
     const categoryProducts = props.Products.filter(product => product.attributes.category_id == category_id)
     const productCards = categoryProducts.map(product => <ProductCard product={product} key={product.id} />)
     const category = props.Categories.find(category => category.id == category_id)
+    
     return (
         <div className="Products" >
             <h2>{category.attributes.name}</h2>
-            {productCards}
+            {productCards.length > 0 ? productCards : <h3>There isn't any product in this category!</h3>}
         </div>
     )
 }
